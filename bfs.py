7f0a4c6e2d92538a91acb5816971306acf428528
Hari Priya M. <priyaharim07@gmail.com>
2025-10-06 08:54:19 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -2,21 +2,6 @@ from collections import deque
 from collections import defaultdict
 
 
-'''
-V E
-FOR EVERY EDGE
-U V
-7 9
-A B
-A C
-A F
-C E
-C F
-C D
-D E
-D G
-G F
-'''
 def bfs(graph,start,visited,path):
     queue = deque()
     path.append(start)
@@ -24,15 +9,21 @@ def bfs(graph,start,visited,path):
     visited[start] = True
     while len(queue) != 0:
         tmpnode = queue.popleft()
-        #TYPE UR CODE HERE
+        for neighbour in graph[tmpnode]:
+            if visited[neighbour] == False:
+                path.append(neighbour)
+                queue.append(neighbour)
+                visited[neighbour] = True
     return path
 
 graph = defaultdict(list)
 v,e = map(int,input().split())
 for i in range(e):
-    #TYOE UR CODE HERE
+    u,v = map(str,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-start = '0'
+start = 'A'
 path = []
 visited = defaultdict(bool)
 traversedpath = bfs(graph,start,visited,path)
